d59b521c79c1aa393fa5598e3207e7142fd2f23a
Alex Santos <alexdsantosv@gmail.com>
2022-02-23 14:12:56 -0600

    [ Init ] first class

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    // STRING | INT | BOOLEAN | CHAR | DOUBLE
+    int edad = 20;
+    int constante = 2;
+
+    // OPERACIONES
+    // suma +
+    // resta -
+    // multiplicacion *
+    // division /
+    System.out.println((edad + constante + (edad * constante) - (constante - edad)) * constante);
+  }
+}
