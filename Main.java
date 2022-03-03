87f9fe0443c004d4e449593426261a3684c0b4f3
Alex Santos <alexdsantosv@gmail.com>
2022-03-02 21:16:14 -0600

    Operaciones relacionales

--- Main.java
+++ Main.java
@@ -1,14 +1,56 @@
 public class Main {
   public static void main(String[] args) {
     // STRING | INT | BOOLEAN | CHAR | DOUBLE
-    int edad = 20;
-    int constante = 2;
 
-    // OPERACIONES
+    String texto1 = "Hola";
+    String texto2 = "Hola";
+
+    // OPERACIONES ALGEBRAICAS -> NUMBER | DECIMAL
     // suma +
     // resta -
     // multiplicacion *
     // division /
-    System.out.println((edad + constante + (edad * constante) - (constante - edad)) * constante);
+    // System.out.println((edad + constante + (edad * constante) - (constante -
+    // edad)) * constante);
+
+    // OPERACIONES RELACIONES -> BOOLEAN
+    // igual ==
+    // no igual !=
+
+    // IF
+
+    // if (edad == constante) {
+    // System.out.println("SI entro en la codicion");
+    // } else {
+    // System.out.println("NO entro en la codicion");
+    // }
+
+    int edad = 20;
+
+    if (edad >= 20) {
+      System.out.println("Adulto");
+    } else {
+      System.out.println("Joven");
+    }
+
+    // switch (edad) {
+    // case 30:
+    // System.out.println("Adulto");
+    // break;
+
+    // case 20:
+    // System.out.println("Joven");
+    // break;
+
+    // case 10:
+    // System.out.println("Niño");
+    // break;
+    // }
+
+    // mayor o igual >=
+    // menor o igual <=
+    // mayor >
+    // menor <
+
   }
 }
